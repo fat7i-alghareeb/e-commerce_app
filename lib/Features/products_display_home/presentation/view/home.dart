40543d87ef6844a5d5e866123331099e908b185a
fat7i-alghareeb <fat7ighareeb@gmail.com>
2024-07-29 19:16:07 +0300

    add liquid pull to refresh

--- lib/Features/products_display_home/presentation/view/home.dart
+++ lib/Features/products_display_home/presentation/view/home.dart
@@ -5,26 +5,34 @@ import '../manger/products cubit/products_cubit.dart';
 import '../manger/products cubit/products_state.dart';
 import 'widgets/home widgets/main_screen_shimmer.dart';
 import 'widgets/home_body.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ProductsCubit, ProductsState>(
-      builder: (context, state) {
-        if (state is ProductsLoaded) {
-          return HomeBody(
-            products: state.products,
-          );
-        } else if (state is ProductsError) {
-          return Center(
-            child: Text(state.errorMessage),
-          );
-        } else {
-          return const MainScreenShimmer();
-        }
+    return LiquidPullToRefresh(
+      onRefresh: () {
+        return BlocProvider.of<ProductsCubit>(context).getAllProducts();
       },
+      animSpeedFactor: 3,
+      showChildOpacityTransition: false,
+      child: BlocBuilder<ProductsCubit, ProductsState>(
+        builder: (context, state) {
+          if (state is ProductsLoaded) {
+            return HomeBody(
+              products: state.products,
+            );
+          } else if (state is ProductsError) {
+            return Center(
+              child: Text(state.errorMessage),
+            );
+          } else {
+            return const MainScreenShimmer();
+          }
+        },
+      ),
     );
   }
 }
